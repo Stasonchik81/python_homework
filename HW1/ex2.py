5c4427a1b8048c06befdcf0b3452d1e8f91c3b70
stanislav <stanislavmuratov@yandex.ru>
2022-11-22 20:48:28 +0300

    add ex2

---
+++ HW1/ex2.py
@@ -0,0 +1,14 @@
+table_of_checks = [(False, False, False),
+                    (False, False, True),
+                    (False, True, False), 
+                    (False, True, True),
+                    (True, False, False),
+                    (True, False, True),
+                    (True, True, False),
+                    (True, True, True),]
+def checkIt(x, y, z):
+    return not (x or y or z) == (not x) and (not y) and (not z)
+
+for i in table_of_checks:
+    print(checkIt(i[0], i[1], i[2]))
+
