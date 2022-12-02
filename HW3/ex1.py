ab6b510f8f3dee078edce6046a2968613f78f770
stanislav <stanislavmuratov@yandex.ru>
2022-12-02 09:28:32 +0300

    Add ex1 & ex2

--- HW3/ex1.py
+++ HW3/ex1.py
@@ -3,4 +3,14 @@
 from My_modules import my_lib
 
 my_list = my_lib.rand_list(5, 1, 10)
-print(my_list)
+
+def sum_of_odd(list_: [int]):
+    result = 0
+    elements = []
+    for i in range(1, len(list_) - 1, 2):
+        result += list_[i]
+        elements.append(list_[i])
+    return (result, elements)
+
+my_result = sum_of_odd(my_list)
+print(f"{my_list} -> на нечётных позициях элементы {my_result[1]}, ответ {my_result[0]}")
---
+++ HW3/ex2.py
@@ -0,0 +1,17 @@
+# multiple pare
+
+from My_modules import my_lib
+
+my_list = my_lib.rand_list(7, 1, 10)
+
+def multi_pare_of_elements(list_):
+    result = []
+    len_ = len(list_)
+    for i in range(len_):
+        if i >= len_/2:
+            return result
+        multi_ = list_[i]*list_[len_-1-i]
+        result.append(multi_)
+
+
+print(f"{my_list} => {multi_pare_of_elements(my_list)}")
